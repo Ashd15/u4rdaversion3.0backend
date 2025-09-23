db52139c004541b037da02b554285c79a3ba9e0a
Akash Dwivedi <akashdwivedi892@gmail.com>
2025-09-23 11:41:56 +0530

    change in model

--- api/models/Coordinator.py
+++ api/models/Coordinator.py
@@ -5,14 +5,17 @@ class Coordinator(models.Model):
     last_name = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
+    about = models.TextField(null=True, blank=True)  # short bio/about section
+    profile_pic = models.ImageField(
+        upload_to="coordinator_profiles/", null=True, blank=True
+    )
 
-    # @staticmethod
-    # def get_coordinator_first_name(email):
-    #     coordinator = Coordinator.objects.filter(email=email).first()
-    #     if coordinator:
-    #         return coordinator.first_name
-    #     return None
+    # TAT (Turnaround Time) stats
+    tat_completed = models.PositiveIntegerField(default=0)
+    tat_breached = models.PositiveIntegerField(default=0)
 
+    created_at = models.DateTimeField(auto_now_add=True)  # track when added
+    updated_at = models.DateTimeField(auto_now=True)      # track last update
 
 
     def __str__(self):
